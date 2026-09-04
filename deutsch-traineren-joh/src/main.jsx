e2e530a4bf370519ce87913683aa6909b231ca8e
Joh3BL <121747493+Joh3BL@users.noreply.github.com>
2026-09-04 08:19:23 +0200

    Changed to template app

--- deutsch-traineren-joh/src/main.jsx
+++ deutsch-traineren-joh/src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import MyApp from './MyApp.jsx'
+// import MyApp from './MyApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MyApp />
+    <App />
   </StrictMode>,
 )
